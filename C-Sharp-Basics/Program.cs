399f6bf997ab5fcd09c616f7d410ffcc8ad9a821
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2021-07-28 20:21:37 +0530

    Type Conversion and Variables in Console Application

---
+++ C-Sharp-Basics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C_Sharp_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String firstNumber = "25";
+            String secondNumber = "12";
+            System.Console.WriteLine((Convert.ToInt16(firstNumber))* Convert.ToInt32(secondNumber));
+        }
+    }
+}
