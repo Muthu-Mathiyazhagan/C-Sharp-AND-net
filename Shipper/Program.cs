dc7d4a1de1f6ef28b1c81c6bf202a3139993e54a
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2021-07-29 22:33:19 +0530

    Enum Working Fine but File Name is not ENUM: May keyword i guess but am not sure now

---
+++ Shipper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Shipper
+{
+    enum ShippingMethod{
+        Local=1,Express=2,Air=4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Air Enum ="+(int)ShippingMethod.Air);
+            var method = 2;
+            System.Console.WriteLine((ShippingMethod)method);
+
+        }
+    }
+}
