ef44f409da5255ad965dc812373a4a1274457a19
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2021-07-29 11:55:50 +0530

    Static Modifiers and Public Methods

---
+++ StaticModifiers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // StaticModifiers.Calculator.Add(2,5);
+            // System.Console.WriteLine(int Calculator.Add (6,5)); 
+            // addition.Add(1,4);
+            var add = new addition();
+            add.Add(5,6);
+        }
+    }
+}
---
+++ StaticModifiers/addition.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace StaticModifiers
+{
+    public class addition
+    {
+        public void Add(int a,int b)
+        {
+            System.Console.WriteLine(a+b);
+            // StaticModifiers.Calculator.Add(2,5);
+            // System.Console.WriteLine(int Calculator.Add (6,5)); 
+        }
+    }
+}
