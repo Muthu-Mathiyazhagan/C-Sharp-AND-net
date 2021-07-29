d8591684feeba303f2f5b819f937ca77e109d031
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2021-07-29 08:42:41 +0530

    Non Primitive Types Such as Classes, ENum and More

---
+++ non-primitive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace non_primitive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
