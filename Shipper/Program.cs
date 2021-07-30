0b2c93ebaf27b28dcba40ce3222b352f4673761d
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2021-07-30 07:51:06 +0530

    Enum Parsing Updated

--- Shipper/Program.cs
+++ Shipper/Program.cs
@@ -2,7 +2,8 @@
 
 namespace Shipper
 {
-    public enum ShippingMethod{
+    public enum ShippingMethod : byte
+    {
         Local=1,Express=2,Air=4
     }
     class Program
@@ -11,9 +12,13 @@ namespace Shipper
         {
             Console.WriteLine("Muthu is the best fullstack Developer!");
             System.Console.WriteLine("Air Enum ="+(int)ShippingMethod.Air);
+            var method = ShippingMethod.Express;
+            System.Console.WriteLine((int)method);
             var methodID = 2;
             System.Console.WriteLine((ShippingMethod)methodID);
-
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod),methodName);
+            System.Console.WriteLine(shippingMethod);
         }
     }
 }
