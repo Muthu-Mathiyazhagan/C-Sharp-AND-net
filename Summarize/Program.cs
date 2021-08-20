c726d25dcc2a947fcb622058105e35194b960a43
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2021-08-20 16:22:53 +0530

    Summarizing the Sentence Which is used to maintain User Interface

---
+++ Summarize/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Summarize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            var sentence = "This is the really big text with focused of the day india of kanniseri pudhur";
+            var summary = StringUtility.SummarizeText(sentence, 40);
+            System.Console.WriteLine(summary);
+        }
+    }
+}
---
+++ Summarize/StringUtility.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+namespace Summarize
+{
+    public class StringUtility
+    {
+        public static string SummarizeText(string text, int maxLength = 20)
+        {
+            System.Console.WriteLine(text);
+
+            if (text.Length < maxLength) return text;
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength) break;
+
+            }
+
+            return String.Join(" ", summaryWords) + "...";
+
+        }
+    }
+}
